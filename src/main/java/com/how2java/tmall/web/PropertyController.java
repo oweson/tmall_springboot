4e8d6a728e82aed0219cab1fc50c5d21041fb126
oweson <570347720@qq.com>
2020-05-19 21:24:35 +0800

    add

--- src/main/java/com/how2java/tmall/web/PropertyController.java
+++ src/main/java/com/how2java/tmall/web/PropertyController.java
@@ -1,8 +1,4 @@
-/**
-* 模仿天猫整站 springboot 教程 为 how2j.cn 版权所有
-* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
-* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
-*/	
+
 
 package com.how2java.tmall.web;
 
@@ -11,38 +7,58 @@ import com.how2java.tmall.service.PropertyService;
 import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
- 
+
+/**
+ * 属性管理
+ */
 @RestController
 public class PropertyController {
-	@Autowired PropertyService propertyService;
+    @Autowired
+    PropertyService propertyService;
 
+    /**
+     * 1 分类下所有的属性
+     */
     @GetMapping("/categories/{cid}/properties")
-    public Page4Navigator<Property> list(@PathVariable("cid") int cid, @RequestParam(value = "start", defaultValue = "0") int start,@RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
-    	start = start<0?0:start;
-    	Page4Navigator<Property> page =propertyService.list(cid, start, size,5); 
+    public Page4Navigator<Property> list(@PathVariable("cid") int cid, @RequestParam(value = "start", defaultValue = "0")
+            int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+        start = start < 0 ? 0 : start;
+        Page4Navigator<Property> page = propertyService.list(cid, start, size, 5);
         return page;
     }
-    
+
+    /**
+     * 2 查看属性
+     */
     @GetMapping("/properties/{id}")
     public Property get(@PathVariable("id") int id) throws Exception {
-    	Property bean=propertyService.get(id);
-    	return bean;
+        Property bean = propertyService.get(id);
+        return bean;
     }
-    
-    
+
+    /**
+     * 3 添加属性
+     */
     @PostMapping("/properties")
     public Object add(@RequestBody Property bean) throws Exception {
         propertyService.add(bean);
         return bean;
     }
-    
+
+    /**
+     * 4 删除属性
+     */
     @DeleteMapping("/properties/{id}")
-    public String delete(@PathVariable("id") int id, HttpServletRequest request)  throws Exception {
+    public String delete(@PathVariable("id") int id, HttpServletRequest request) throws Exception {
         propertyService.delete(id);
         return null;
     }
-    
+
+    /**
+     * 5 更新属性
+     */
     @PutMapping("/properties")
     public Object update(@RequestBody Property bean) throws Exception {
         propertyService.update(bean);
@@ -50,12 +66,11 @@ public class PropertyController {
     }
 
 
-
 }
 
 
 /**
-* 模仿天猫整站 springboot 教程 为 how2j.cn 版权所有
-* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
-* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
-*/	
+ * 模仿天猫整站 springboot 教程 为 how2j.cn 版权所有
+ * 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+ * 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+ */
