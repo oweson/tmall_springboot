8373da995c9cad9ae2bb6e5b1afce8d21c245ac9
oweson <570347720@qq.com>
2020-05-23 19:39:10 +0800

    update sth

--- src/main/java/com/how2java/tmall/config/RedisConfig.java
+++ src/main/java/com/how2java/tmall/config/RedisConfig.java
@@ -1,10 +1,7 @@
-/**
-* 模仿天猫整站 springboot 教程 为 how2j.cn 版权所有
-* 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
-* 供购买者学习，请勿私自传播，否则自行承担相关法律责任
-*/	
+
 
 package com.how2java.tmall.config;
+
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -19,13 +16,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
- 
+
 @Configuration
-//Redis 缓存配置类
+/**Redis 缓存配置类,兼容stringRedisTemplate和redisTemplate*/
 public class RedisConfig extends CachingConfigurerSupport {
- 
+
     @Bean
-    public CacheManager cacheManager(RedisTemplate<?,?> redisTemplate) {
+    public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
         RedisSerializer stringSerializer = new StringRedisSerializer();
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
@@ -33,13 +30,13 @@ public class RedisConfig extends CachingConfigurerSupport {
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
         redisTemplate.setKeySerializer(stringSerializer);
-        redisTemplate.setHashKeySerializer(stringSerializer);  
-         
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);         
+        redisTemplate.setHashKeySerializer(stringSerializer);
+
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
         redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
         CacheManager cacheManager = new RedisCacheManager(redisTemplate);
         return cacheManager;
-  
+
     }
 }
 
